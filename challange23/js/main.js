c3f4bb80230d680debb6ba34b4dd207c696a1d55
bpor92 <bartoszporebski1992@gmail.com>
2016-12-04 11:25:09 +0100

    fixed menu, buttons

--- challange23/js/main.js
+++ challange23/js/main.js
@@ -8,8 +8,23 @@ function myFunction() {
     }
 }
 
+
 $(document).on('click', function (event) {
         if ($(event.target).closest('#myTopnav').length == false) {
             $(".topnav__list").removeClass('responsive');
+            
         }
     });
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('#myTopnav').removeClass('responsive');
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1300);
+    }
+});
