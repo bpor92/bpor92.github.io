1946eb132aedd1de41354ae3926811951dc7ba70
bpor92 <bartoszporebski1992@gmail.com>
2016-12-05 16:32:22 +0100

    fix navbar

--- challange23/js/main.js
+++ challange23/js/main.js
@@ -1,30 +1,48 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav__list") {
-        x.className += " responsive";
+$(function(){
 
-    } else {
-        x.className = "topnav__list";
-    }
-}
+    $('.btn-hamburger').on('click', function(){
+        $('#myTopnav').toggleClass('responsive');
+    });
 
 
-$(document).on('click', function (event) {
-        if ($(event.target).closest('#myTopnav').length == false) {
-            $(".topnav__list").removeClass('responsive');
-            
+    $(window).on('scroll', function() {
+                $('.topnav__item a').each(function(index, value) {
+                    var target = value.hash,
+                        start = $(target).offset().top,
+                        end = start + $(target).outerHeight(),
+                        scrollPosition = $(window).scrollTop() + 90;     
+                    if( (start <=  scrollPosition) && (scrollPosition <= end) ) {
+                        $(value).closest('a').addClass('active');
+                    }else {
+                        $(value).closest('a').removeClass('active');
+                    }
+                    if(target === '#about'){
+                        $('a[href$="home"]').removeClass('active')
+                    }
+                });
+            });
+
+    $(document).on('click', function (event) {
+            if ($(event.target).closest('#myTopnav').length == false) {
+                $(".topnav__list").removeClass('responsive');
+                
+            }
+        });
+
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        $('.topnav__item a').removeClass('active')
+        $(this).addClass('active');
+        if( target.length ) {
+            event.preventDefault();
+            $('#myTopnav').removeClass('responsive');
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1300);
         }
     });
 
-$('a[href^="#"]').on('click', function(event) {
 
-    var target = $(this.getAttribute('href'));
-
-    if( target.length ) {
-        event.preventDefault();
-        $('#myTopnav').removeClass('responsive');
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1300);
-    }
 });
+
+
